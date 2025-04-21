386ee819768014ed2207c9db38821d45cfaa9c31
Sharayu Mane <manesharayu16@gmail.com>
2025-04-21 21:32:42 +0530

    get neeting details based on emailid and status

--- src/main/java/com/schedulo/schedulo/repository/MeetingParticipantsMappingRepository.java
+++ src/main/java/com/schedulo/schedulo/repository/MeetingParticipantsMappingRepository.java
@@ -4,7 +4,10 @@ import com.schedulo.schedulo.entity.ParticipantsMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MeetingParticipantsMappingRepository extends JpaRepository<ParticipantsMeeting,Long> {
+    List<ParticipantsMeeting> findByParticipantsMailId(String emailId);
 
 }
--- src/main/java/com/schedulo/schedulo/repository/MeetingProviderMappingRepository.java
+++ src/main/java/com/schedulo/schedulo/repository/MeetingProviderMappingRepository.java
@@ -1,8 +1,12 @@
 package com.schedulo.schedulo.repository;
 
+
 import com.schedulo.schedulo.entity.ProviderMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MeetingProviderMappingRepository extends JpaRepository<ProviderMeeting, Long> {
+
 
 }
